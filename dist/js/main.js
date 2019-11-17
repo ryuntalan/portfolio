2be2a437843c7052044bfb219b02ddde4d64f201
Ryan Untalan <untalan.ryan@gmail.com>
2019-11-16 22:14:44 -0800

    updated the whole website

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".container", {
+  duration: 2000,
+  viewFactor: 0.2
+});
+
+sr.reveal(".btn", {
+  duration: 2000,
+  delay: 2000,
+  origin: "bottom"
+});
