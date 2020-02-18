0d460cbed87589feb16d1b926ce26e768a3c0cb4
Ryan Untalan <untalan.ryan@gmail.com>
2020-02-17 16:25:15 -0800

    arrow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// scroll reveal
 window.sr = ScrollReveal();
 
 sr.reveal(".container", {
@@ -11,37 +12,13 @@ sr.reveal(".btn", {
   origin: "bottom"
 });
 
-//Select DOM Items
+sr.reveal("demo", {
+  duration: 2000,
+  viewFactor: 0.2
+});
 
-const menuBtn = document.querySelector(".menu-btn");
-
-const menu = document.querySelector(".menu");
-
-const menuNav = document.querySelector(".menu-nav");
-
-const navItems = document.querySelectorAll(".nav-item");
-
-//Set Initial State of Menu
-let showMenu = false;
-
-menuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
-
-    // Set Menu State
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
-
-    // Set Menu State
-    showMenu = false;
+$(window).bind("scroll", function() {
+  if ($(window).scrollTop() > 100) {
+    $("#arrow").hide("slow");
   }
-}
+});
