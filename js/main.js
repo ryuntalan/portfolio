0d2954600d5ffd9897fc1dd9c567fd95664008b3
Ryan Untalan <untalan.ryan@gmail.com>
2020-09-02 15:14:45 -0700

    jupyter case study

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ window.sr = ScrollReveal();
 sr.reveal(".btn", {
   duration: 2000,
   delay: 2000,
-  origin: "bottom"
+  origin: "bottom",
 });
 
 sr.reveal("demo", {
   duration: 2000,
-  viewFactor: 0.8
+  viewFactor: 0.8,
 });
 
-$(window).bind("scroll", function() {
+$(window).bind("scroll", function () {
   if ($(window).scrollTop() > 100) {
     $("#arrow").hide("slow");
   }
@@ -38,7 +38,7 @@ function toggleMenu() {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
@@ -46,7 +46,7 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
@@ -57,7 +57,7 @@ function toggleMenu() {
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
 };
 
@@ -102,4 +102,74 @@ if (currentTheme) {
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
   }
+}
+
+//SLIDESHOW
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//Showcase2
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides2.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides2.length;
+  }
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
 }
